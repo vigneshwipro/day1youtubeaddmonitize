cfcadd7d054b8d787e486cf3634be0630507d36a
vigneshwipro <vignesh231097@gmail.com>
2023-09-30 22:03:30 +0530

    Create a File

---
+++ day4project.py
@@ -0,0 +1,151 @@
+c = {
+    "Link-1" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-2" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-3" : 
+        {
+            "Lang"     : "English" ,
+            "Add_Name" : "Rifik" ,
+            "Add_Time" : "02:30 Minutes"
+        } ,
+    "Link-4" : 
+        {
+            "Lang"     : "English" ,
+            "Add_Name" : "Firebase" ,
+            "Add_Time" : "22 Seconds"
+        } ,
+    "Link-5" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-6" : 
+        {
+            "Lang"     : "English" ,
+            "Add_Name" : "Rifik" ,
+            "Add_Time" : "02:29 Minutes"
+        } ,
+    "Link-7" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-8" : 
+        {
+            "Lang"     : "English" ,
+            "Add_Name" : "Instagram" ,
+            "Add_Time" : "12 Seconds"
+        } ,
+    "Link-9" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-10" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        }
+}
+print("Day 3 :\n")
+print("Link-1 \n\tLang     :",c["Link-1"]["Lang"],"\n\tAdd_Name :",c["Link-1"]["Add_Name"],"\n\tAdd_Time :",c["Link-1"]["Add_Time"],"\n")
+print("Link-2 \n\tLang     :",c["Link-2"]["Lang"],"\n\tAdd_Name :",c["Link-2"]["Add_Name"],"\n\tAdd_Time :",c["Link-2"]["Add_Time"],"\n")
+print("Link-3 \n\tLang     :",c["Link-3"]["Lang"],"\n\tAdd_Name :",c["Link-3"]["Add_Name"],"\n\tAdd_Time :",c["Link-3"]["Add_Time"],"\n")
+print("Link-4 \n\tLang     :",c["Link-4"]["Lang"],"\n\tAdd_Name :",c["Link-4"]["Add_Name"],"\n\tAdd_Time :",c["Link-4"]["Add_Time"],"\n")
+print("Link-5 \n\tLang     :",c["Link-5"]["Lang"],"\n\tAdd_Name :",c["Link-5"]["Add_Name"],"\n\tAdd_Time :",c["Link-5"]["Add_Time"],"\n")
+print("Link-6 \n\tLang     :",c["Link-6"]["Lang"],"\n\tAdd_Name :",c["Link-6"]["Add_Name"],"\n\tAdd_Time :",c["Link-6"]["Add_Time"],"\n")
+print("Link-7 \n\tLang     :",c["Link-7"]["Lang"],"\n\tAdd_Name :",c["Link-7"]["Add_Name"],"\n\tAdd_Time :",c["Link-7"]["Add_Time"],"\n")
+print("Link-8 \n\tLang     :",c["Link-8"]["Lang"],"\n\tAdd_Name :",c["Link-8"]["Add_Name"],"\n\tAdd_Time :",c["Link-8"]["Add_Time"],"\n")
+print("Link-9 \n\tLang     :",c["Link-9"]["Lang"],"\n\tAdd_Name :",c["Link-9"]["Add_Name"],"\n\tAdd_Time :",c["Link-9"]["Add_Time"],"\n")
+print("Link-10 \n\tLang     :",c["Link-10"]["Lang"],"\n\tAdd_Name :",c["Link-10"]["Add_Name"],"\n\tAdd_Time :",c["Link-10"]["Add_Time"],"\n\n\n")
+d = {
+    "Link-1" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-2" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-3" : 
+        {
+            "Lang"     : "Hindi" ,
+            "Add_Name" : "Google pay" ,
+            "Add_Time" : "09 Seconds"
+        } ,
+    "Link-4" : 
+        {
+            "Lang"     : "English" ,
+            "Add_Name" : "Masterclass" ,
+            "Add_Time" : "36 Seconds"
+        } ,
+    "Link-5" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-6" : 
+        {
+            "Lang"     : "English" ,
+            "Add_Name" : "Rifik" ,
+            "Add_Time" : "02:30 Minutes"
+        } ,
+    "Link-7" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-8" : 
+        {
+            "Lang"     : "Hindi" ,
+            "Add_Name" : "Amazon" ,
+            "Add_Time" : "06 Seconds"
+        } ,
+    "Link-9" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        } ,
+    "Link-10" : 
+        {
+            "Lang"     : "0" ,
+            "Add_Name" : "0" ,
+            "Add_Time" : "0"
+        }
+}
+print("Day 4 :\n")
+print("Link-1 \n\tLang     :",d["Link-1"]["Lang"],"\n\tAdd_Name :",d["Link-1"]["Add_Name"],"\n\tAdd_Time :",d["Link-1"]["Add_Time"],"\n")
+print("Link-2 \n\tLang     :",d["Link-2"]["Lang"],"\n\tAdd_Name :",d["Link-2"]["Add_Name"],"\n\tAdd_Time :",d["Link-2"]["Add_Time"],"\n")
+print("Link-3 \n\tLang     :",d["Link-3"]["Lang"],"\n\tAdd_Name :",d["Link-3"]["Add_Name"],"\n\tAdd_Time :",d["Link-3"]["Add_Time"],"\n")
+print("Link-4 \n\tLang     :",d["Link-4"]["Lang"],"\n\tAdd_Name :",d["Link-4"]["Add_Name"],"\n\tAdd_Time :",d["Link-4"]["Add_Time"],"\n")
+print("Link-5 \n\tLang     :",d["Link-5"]["Lang"],"\n\tAdd_Name :",d["Link-5"]["Add_Name"],"\n\tAdd_Time :",d["Link-5"]["Add_Time"],"\n")
+print("Link-6 \n\tLang     :",d["Link-6"]["Lang"],"\n\tAdd_Name :",d["Link-6"]["Add_Name"],"\n\tAdd_Time :",d["Link-6"]["Add_Time"],"\n")
+print("Link-7 \n\tLang     :",d["Link-7"]["Lang"],"\n\tAdd_Name :",d["Link-7"]["Add_Name"],"\n\tAdd_Time :",d["Link-7"]["Add_Time"],"\n")
+print("Link-8 \n\tLang     :",d["Link-8"]["Lang"],"\n\tAdd_Name :",d["Link-8"]["Add_Name"],"\n\tAdd_Time :",d["Link-8"]["Add_Time"],"\n")
+print("Link-9 \n\tLang     :",d["Link-9"]["Lang"],"\n\tAdd_Name :",d["Link-9"]["Add_Name"],"\n\tAdd_Time :",d["Link-9"]["Add_Time"],"\n")
+print("Link-10 \n\tLang     :",d["Link-10"]["Lang"],"\n\tAdd_Name :",d["Link-10"]["Add_Name"],"\n\tAdd_Time :",d["Link-10"]["Add_Time"],"\n\n\n")
+print("Result :")
+if c==d:
+    print("\tSame")
+else:
+    print("\tNot Same")
